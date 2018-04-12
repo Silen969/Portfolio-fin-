ff999d05c2fd058ce20980c5976a64f45607e701
Silen969 <Silen651@gmail.com>
2018-04-12 20:27:31 +0300

    logic

---
+++ docs/logic/js/navigation.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+function openNav2() {
+    document.getElementById("mySidenav2").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+}
